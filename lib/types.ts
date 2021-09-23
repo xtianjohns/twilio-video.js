ea574889dfbb24c1df090e1f3073fe7e471841fd
joma <joma@twilio.com>
2021-09-22 21:00:26 -0700

    Saving progress

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+declare module '@twilio/webrtc' {
+  interface MediaStreamTrack {
+    foo: string;
+  }
+}
+
+declare module '@twilio/webrtc/lib/util' {
+  function guessBrowser(userAgent?: string) : string;
+  function guessBrowserVersion(userAgent?: string) : {major: number, minor: number} | undefined;
+}
