03bd690a470cacbee2c69e5e0481294979124ddd
Makarand Patwardhan <mpatwardhan@twilio.com>
2021-11-10 18:33:35 -0800

    more instrumentation

--- docker/test.js
+++ docker/test.js
@@ -1,5 +1,9 @@
+var os = require('os');
+
 const Server = require("./dockerProxyServer.js");
 async function main() {
+  console.log('os.userInfo:', os.userInfo());
+  console.log('os.homedir:', os.homedir());
   const server = new Server();
   console.log('starting server...');
   await server.startServer();
